04b242e3f0a3366b3b80fceb79e2b9f6be042b6b
S-TamLTM <iosblurgroup@gmail.com>
2016-07-13 21:20:31 -0700

    Check uiview hide show in date picker.

--- Picker/Picker/HomeViewController/HomeViewController.swift
+++ Picker/Picker/HomeViewController/HomeViewController.swift
@@ -12,16 +12,51 @@ class HomeViewController: UIViewController {
     
     @IBOutlet weak private var dateTimeLabel: UILabel!
     @IBOutlet weak private var myDatePicker: UIDatePicker!
+    @IBOutlet weak private var hiddenDatePickerUiView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpUI()
+        setUpData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    //Action button
     @IBAction func displayButtonTappedPicker(sender: AnyObject) {
         dateTimeLabel.text = NSDateFormatter.localizedStringFromDate(myDatePicker.date, dateStyle: NSDateFormatterStyle.FullStyle, timeStyle: NSDateFormatterStyle.ShortStyle)
+        hiddenDatePickerUiView.hidden = true
+    }
+    
+    private func setUpUI() {
+        configureHomeViewController()
+        tapUILabel()
+        hiddenDatePickerUiView.hidden = true
+    }
+    
+    private func setUpData() {
+        
+    }
+    
+    private func configureHomeViewController() {
+        self.title = "Home"
+    }
+    
+    private func tapUILabel() {
+        let tapDateTimeLabel = UITapGestureRecognizer(target: self, action: #selector(openPickerView(_:)))
+        dateTimeLabel.userInteractionEnabled = true
+        dateTimeLabel.addGestureRecognizer(tapDateTimeLabel)
+    }
+    
+    @objc private func openPickerView(sender: UIGestureRecognizer) {
+        UIView.animateWithDuration(0.4, animations: {
+            if self.hiddenDatePickerUiView.hidden == true {
+                self.hiddenDatePickerUiView.hidden = false
+            } else {
+                self.hiddenDatePickerUiView.hidden = true
+            }
+        }, completion: (nil))
     }
 }
